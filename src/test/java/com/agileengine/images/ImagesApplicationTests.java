9954660db6b19b27652680eb716efdb450e8317a
e.petrov@flex-solution.com <evgeniy.jyk@gmail.com>
2020-12-15 00:21:33 +0200

    initial commit

---
+++ src/test/java/com/agileengine/images/ImagesApplicationTests.java
@@ -0,0 +1,14 @@
+package com.agileengine.images;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
